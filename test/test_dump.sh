97c0e50c6b4b3e9a2ee1f0dd01ca7382cbf85586
dabercro <dabercro@mit.edu>
2018-10-02 22:30:31 -0400

    Add way to quickly change dumped tree's name

--- test/test_dump.sh
+++ test/test_dump.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+test ! -d var || rm -rf var
+
 if which coverage
 then
     coverage run --source dynamo_consistency -a $(which consistency-dump-tree) --test --site TEST_SITE
@@ -7,6 +9,7 @@ else
     consistency-dump-tree --test --site TEST_SITE
 fi
 
-diff dumptest.txt <(python -c "from dynamo_consistency.datatypes import get_info; get_info('var/cache/TEST_SITE/inventory.pkl').display()")
+diff dumptest.txt <(python -c "from dynamo_consistency.datatypes import get_info; get_info('var/cache/TEST_SITE/inventory.pkl').display()") || exit 1
 
-exit $?
+consistency-dump-tree --test --site TEST_SITE funny_name
+diff dumptest.txt <(python -c "from dynamo_consistency.datatypes import get_info; get_info('var/cache/TEST_SITE/funny_name.pkl').display()") || exit 2
